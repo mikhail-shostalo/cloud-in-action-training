64ac66ab6b828c28d47f7f1d2e5ac94aacb26e3a
Mykhailo Shostalo <mykhailo_shostalo@epam.com>
2021-02-23 11:28:09 +0200

    Added azure pipelines yaml

--- cloud-server/src/main/java/com/training/cloud/controller/HealthCheckController.java
+++ cloud-server/src/main/java/com/training/cloud/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.training.cloud.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+    @Value("${message.for.testing}")
+    private String message;
+
     @GetMapping("/health-check")
     public ResponseEntity<String> getHealthCheck() {
-        return new ResponseEntity<>("Welcome to my site!", HttpStatus.OK);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
