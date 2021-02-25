0802f9b44551689090f7d38be0cc5cd7b0fb0cc9
Mykhailo Shostalo <mykhailo_shostalo@epam.com>
2021-02-25 12:53:31 +0200

    Changed property value Added unit test

--- cloud-server/src/test/java/com/training/cloud/CloudApplicationTests.java
+++ cloud-server/src/test/java/com/training/cloud/CloudApplicationTests.java
@@ -1,13 +1,40 @@
 package com.training.cloud;
 
+import com.training.cloud.controller.HealthCheckController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "/application-test.properties")
 class CloudApplicationTests {
 
+	@Autowired
+	private HealthCheckController healthCheckController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(healthCheckController).isNotNull();
+	}
+
+	@Test
+	public void shouldReturnMessageFromProperties() throws Exception {
+		mockMvc.perform(get("/health-check")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("Test running"));
 	}
 
 }
